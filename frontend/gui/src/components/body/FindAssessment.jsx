6623dd39e91cdc76d0c0d5f2b2d7a36926399432
Techoplite <oriccimirko@gmail.com>
2020-06-10 09:21:48 +0100

    Add fecth assessment in find assessment form

--- frontend/gui/src/components/body/FindAssessment.jsx
+++ frontend/gui/src/components/body/FindAssessment.jsx
@@ -3,25 +3,50 @@ import React, { Component, Fragment } from "react";
 class FindAssessment extends Component {
   constructor(props) {
     super(props);
-    this.state = { assessmentId: null };
+    this.state = {
+      assessment: null,
+    };
+    this.fetchAssessment = this.fetchAssessment.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
+  async fetchAssessment() {
+    const id = parseInt(this.state.assessmentId);
+    try {
+      await fetch(`http://127.0.0.1:8000/api/assessments/${id}/`)
+        .then(response => response.json())
+        .then(data => this.setState({ assessment: data }));
+    } catch (err) {
+      document.getElementById("assessmentId").innerHTML = err.message;
+    }
+  }
+
   render() {
     return (
       <Fragment>
         <h1 className="mb-5">Find Assessment</h1>
         <form>
           <div className="form-group">
-            <label htmlFor="formGroupExampleInput" className=" float-left">
+            <label htmlFor="assessmentId" className=" float-left">
               Assessment Id
             </label>
             <input
-              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+              type="search"
               className="form-control"
-              id="formGroupExampleInput"
+              id="assessmentId"
               placeholder="Enter an assessment Id."
             />
           </div>
-          <button type="submit" className="btn btn-primary float-right">
+          <button
+            type="submit"
+            className="btn btn-primary float-right"
+            onClick={this.fetchAssessment}>
             Search
           </button>
         </form>
