13fd4329a09062ffcf48135474b7fab448dc57ae
Techoplite <oriccimirko@gmail.com>
2020-06-19 18:33:37 +0100

    Switch user to Teacher

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     super(props);
     this.state = {
       message: '',
-      userRole: "Student"
+      userRole: "Teacher"
     }
     this.handleMessage = this.handleMessage.bind(this)
   }
